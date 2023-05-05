56e2b207e9d292f69ad8af6105c1c037609faf7a
nisaba <infos@nisaba.solutions>
2023-05-05 11:36:38 +0200

    050523-2

--- BTCPayServer.Plugins.Template/Plugin.cs
+++ BTCPayServer.Plugins.Template/Plugin.cs
@@ -13,7 +13,7 @@ public class Plugin : BaseBTCPayServerPlugin
 {
     public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } = new[]
     {
-        new IBTCPayServerPlugin.PluginDependency { Identifier = nameof(BTCPayServer), Condition = ">=1.8.2" }
+        new IBTCPayServerPlugin.PluginDependency { Identifier = nameof(BTCPayServer), Condition = ">=1.8.1" }
     };
 
     public override void Execute(IServiceCollection services)
