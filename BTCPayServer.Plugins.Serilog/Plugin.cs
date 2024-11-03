269a4d56db6600829842e88a682a90bdeadebd98
Nisaba <cbernardini@nisaba.solutions>
2024-11-03 20:52:09 +0000

    1.2.1

--- BTCPayServer.Plugins.Serilog/Plugin.cs
+++ BTCPayServer.Plugins.Serilog/Plugin.cs
@@ -13,7 +13,7 @@ public class Plugin : BaseBTCPayServerPlugin
 {
     public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } = new[]
     {
-        new IBTCPayServerPlugin.PluginDependency { Identifier = nameof(BTCPayServer), Condition = ">=1.12.0" }
+        new IBTCPayServerPlugin.PluginDependency { Identifier = nameof(BTCPayServer), Condition = ">=2.0.0" }
     };
 
     public override void Execute(IServiceCollection services)
