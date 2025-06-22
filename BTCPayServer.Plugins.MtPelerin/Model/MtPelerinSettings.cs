0402b019176d97ca8127af8eee26ff0a122d1c93
Nisaba <cbernardini@nisaba.solutions>
2025-06-22 17:15:03 +0000

    update mt pelerin

--- BTCPayServer.Plugins.MtPelerin/Model/MtPelerinSettings.cs
+++ BTCPayServer.Plugins.MtPelerin/Model/MtPelerinSettings.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using YamlDotNet.Core.Tokens;
 
 namespace BTCPayServer.Plugins.MtPelerin.Model;
 
@@ -16,7 +18,21 @@ public class MtPelerinSettings
     [Display(Name = "Display language")]
     public string Lang { get; set; }
 
-    [Display(Name = "Your phone")]
+    [Display(Name = "Your phone (numbers only)")]
     public string Phone { get; set; }
+
+    [NotMapped]
+    public int PhoneInt
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(Phone))
+                return 0;
+            var sPhone = Phone.StartsWith("00") ? Phone[2..] : Phone;
+            if (int.TryParse(sPhone, out int phoneInt))
+                return phoneInt;
+            return 0;
+        }
+    }
 
 }
