afbc0e76c9368f48f6a3ff3bb60f3aa3c628b7ba
Nisaba <cbernardini@nisaba.solutions>
2025-06-28 17:40:16 +0000

    update mtpelerin

---
+++ BTCPayServer.Plugins.MtPelerin/Migrations/20250629_Init.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BTCPayServer.Plugins.MtPelerin.Migrations
+{
+    [DbContext(typeof(MtPelerinPluginDbContext))]
+    [Migration("20250629_Init")]
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            try
+            {
+                migrationBuilder.EnsureSchema(
+                    name: "BTCPayServer.Plugins.MtPelerin");
+
+                migrationBuilder.CreateTable(
+                    name: "MtPelerinSettings",
+                    schema: "BTCPayServer.Plugins.MtPelerin",
+                    columns: table => new
+                    {
+                        StoreId = table.Column<string>(nullable: false),
+                        Lang = table.Column<string>(nullable: false),
+                        UseBridgeApp = table.Column<bool>(nullable: false),
+                        Phone = table.Column<string>(nullable: true),
+                    },
+                    constraints: table =>
+                    {
+                        table.PrimaryKey("PK_MtPelerinSettings", x => x.StoreId);
+                    });
+            }
+            catch 
+            {
+                
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MtPelerinSettings",
+                schema: "BTCPayServer.Plugins.MtPelerin");
+        }
+    }
+}
