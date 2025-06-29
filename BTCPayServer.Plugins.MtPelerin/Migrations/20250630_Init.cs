1662b052476304d21ccd90504eea8769076bd0a9
Nisaba <cbernardini@nisaba.solutions>
2025-06-29 15:48:22 +0000

    update mt pelerin

--- BTCPayServer.Plugins.MtPelerin/Migrations/20250630_Init.cs
+++ BTCPayServer.Plugins.MtPelerin/Migrations/20250630_Init.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 namespace BTCPayServer.Plugins.MtPelerin.Migrations
 {
     [DbContext(typeof(MtPelerinPluginDbContext))]
-    [Migration("20250629_Init")]
+    [Migration("20250630_Init")]
     public partial class Init : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
