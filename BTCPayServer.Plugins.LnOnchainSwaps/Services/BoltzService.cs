bbfa8e6a86fa90e1cec595e970603fa7c54a29fc
Nisaba <cbernardini@nisaba.solutions>
2025-08-21 17:19:42 +0000

    update

--- BTCPayServer.Plugins.LnOnchainSwaps/Services/BoltzService.cs
+++ BTCPayServer.Plugins.LnOnchainSwaps/Services/BoltzService.cs
@@ -66,7 +66,7 @@ namespace BTCPayServer.Plugins.LnOnchainSwaps.Services
                     PreImageHash = preImageHash,
                     SwapId = JsonRep.id,
                     Destination = JsonRep.address,
-                    ExpectedAmount = JsonRep.expectedAmount / 100000000,
+                    ExpectedAmount = JsonRep.expectedAmount,
                     Json = sRep
                 };
             }
