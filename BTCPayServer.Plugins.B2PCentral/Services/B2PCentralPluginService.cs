52fd0495d73be0c7c70cce0388c8cebcf1ac422e
Nisaba <cbernardini@nisaba.solutions>
2025-12-26 18:15:17 +0000

    upd

--- BTCPayServer.Plugins.B2PCentral/Services/B2PCentralPluginService.cs
+++ BTCPayServer.Plugins.B2PCentral/Services/B2PCentralPluginService.cs
@@ -391,7 +391,7 @@ public class B2PCentralPluginService(B2PCentralPluginDbContextFactory pluginDbCo
         try
         {
             await using var btcPayCtx = btcPayDbContextFactory.CreateContext();
-            return !await btcPayCtx.Payouts.AnyAsync(a => a => a.PayoutMethodId == "BTC-CHAIN"
+            return !await btcPayCtx.Payouts.AnyAsync(a => a.PayoutMethodId == "BTC-CHAIN"
                                                     && a.State != PayoutState.Cancelled
                                                     && a.State != PayoutState.Completed);
         }
